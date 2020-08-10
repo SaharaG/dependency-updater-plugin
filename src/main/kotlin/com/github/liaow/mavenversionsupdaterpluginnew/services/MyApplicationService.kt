5906ff402408c04f633bb98440aee38cb59051cf
GitHub Action <action@github.com>
2020-08-10 02:38:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/liaow/mavenversionsupdaterpluginnew/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.liaow.mavenversionsupdaterpluginnew.services
+
+import com.github.liaow.mavenversionsupdaterpluginnew.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/liaow/mavenversionsupdaterpluginnew/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liaow/mavenversionsupdaterpluginnew/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.liaow.mavenversionsupdaterpluginnew.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liaow.mavenversionsupdaterpluginnew.MyBundle
 
 class MyProjectService(project: Project) {
 
